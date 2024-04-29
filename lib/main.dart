ae3ec0e3a44dc567b53738d05192c0366e6d6d50
TheRealRaziandJaso <Neno701@outlook.com>
2024-04-29 12:03:20 +1000

    added Paintool class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_box_transform/flutter_box_transform.dart';
 
@@ -33,13 +34,225 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Offset formPosition = Offset.zero;
   bool _isFormVisible = false;
+  void _updateFormVisibility() {
+  setState(() {
+    _isFormVisible = !_isFormVisible;
+  });
+}
+
+  _GridToolState tool = _GridToolState();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Pixel editor canvas
+          Container(
+            color: Colors.white,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          // Movable toolbar
+          CustomToolBar(
+            screenWidth: screenWidth, 
+            screenHeight: screenHeight, 
+            ypos: screenHeight / 4,
+          ),
+          //movable form
+          if (tool.isFormVisibile)
+            Positioned(
+              left: tool.formPosition.dx,
+              top: tool.formPosition.dy,
+              child: GestureDetector(
+                onPanUpdate: tool._handleFormUpdate,
+                child: tool.gridForm(screenWidth, screenHeight),
+              )
+            ),
+        ],
+      ),
+    );
+  }
+
+}
+/////////////CUSTOM TOOLBAR CLASS/////////////////////////
+class CustomToolBar extends StatefulWidget {
+    final double screenWidth;
+    final double screenHeight;
+    final double ypos;
+
+    CustomToolBar({
+        required this.screenWidth,
+        required this.screenHeight,
+        required this.ypos,
+    });
+
+    @override
+    _CustomToolBarState createState() => _CustomToolBarState();
+}
+
+class _CustomToolBarState extends State<CustomToolBar> {
+    Offset toolbarPosition = Offset.zero;
+
+    @override
+    initState() {
+      super.initState();
+      toolbarPosition = Offset(0, widget.ypos);
+    }
+
+    void _handleToolBarUpdate(DragUpdateDetails details) {
+      setState(() {
+        toolbarPosition += details.delta;
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      double width = widget.screenWidth * 0.1;
+      double height = widget.screenHeight * 0.5;
+
+      double threshold = widget.screenHeight * 0.05;
+      double lowerThreshold = widget.screenHeight - threshold - height;
+      bool rotatedBar = false;
+
+      if(toolbarPosition.dy <= threshold || toolbarPosition.dy >= lowerThreshold)
+      {
+        rotatedBar = true;
+      } else {
+        rotatedBar = false;
+      }
+
+        return Positioned(
+            left: toolbarPosition.dx,
+            top: toolbarPosition.dy,
+            child: GestureDetector(
+                onPanUpdate: _handleToolBarUpdate,
+                child: AnimatedRotation(
+                    turns: rotatedBar ? 0.25 : 0, 
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                    child: Container(
+                        width: width,
+                        height: height,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16.0),
+                            color: Colors.grey[400],
+                        ),
+                        child: Padding(
+                            padding: const EdgeInsets.only(top: 12.0),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                    //toolBarButton(Icons.grid_on, widget.toggleFormVisibility, Colors.grey.shade300),
+                                    GridTool(),
+                                    PaintTool(),
+                                ],
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
+}
+
+///////////////MIXIN CLASS//////////////////
+mixin ToolBarButtons {
+  Widget toolBarButton(IconData icon, Function() action, Color color) {
+    return Flexible(
+      flex: 1,
+      child: FractionallySizedBox(
+        widthFactor: 0.75,
+        heightFactor: 0.2,
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              color: color,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: GestureDetector(
+              onTap: action,
+              child: Icon(
+                icon,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+//////////////////////PAINT TOOL CLASS//////////////////////
+class PaintTool extends StatefulWidget with ToolBarButtons {
+
+  @override
+  _PaintToolState createState() => _PaintToolState();
+}
+
+class _PaintToolState extends State<PaintTool> {
+  bool paintSelected = false;
+  Color buttonColor = Colors.grey.shade300;
+
+  void _togglePaintButton() {
+    setState(() {
+      paintSelected = !paintSelected;
+      buttonColor = paintSelected ? Colors.blue.shade300 : Colors.grey.shade300;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Call the inherited toolBarButton function to create the toolbar button
+    return widget.toolBarButton(
+      Icons.format_paint,
+      _togglePaintButton,
+      buttonColor,
+    );
+  }
+}
+////////////////GRID TOOL CLASS///////////////////
+class GridTool extends StatefulWidget with ToolBarButtons{
+
+  /*final double screenWidth;
+  final double screenHeight;
+
+  GridTool({
+      required this.screenWidth,
+      required this.screenHeight,
+  });*/
+
+  @override
+  _GridToolState createState() => _GridToolState();
+}
+
+class _GridToolState extends State<GridTool> {
+  Offset formPosition = Offset(0,0);
+  bool _isFormVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   void _toggleFormVisibility() {
     setState(() {
       _isFormVisible = !_isFormVisible;
     });
+    print(_isFormVisible);
   }
 
   void _handleFormUpdate(DragUpdateDetails details) {
@@ -48,10 +261,14 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Widget gridForm(height){
+  bool get isFormVisibile => _isFormVisible;
+
+  Widget gridForm(screenWidth, screenHeight){
     //final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     final TextEditingController _width = TextEditingController();
     final TextEditingController _height = TextEditingController();
+    formPosition = Offset(screenWidth/2, screenHeight/2);
+    double height = screenHeight * 0.25;
 
     return Container(
       width: height, //both use same value == box shape
@@ -133,171 +350,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-
-    if(formPosition == Offset(0,0)){//initialise form location
-      formPosition = Offset(screenWidth/2, screenWidth/2);
-    }
-    
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Pixel editor canvas
-          Container(
-            color: Colors.white,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          // Movable toolbar
-          CustomToolBar(
-            screenWidth: screenWidth, 
-            screenHeight: screenHeight, 
-            ypos: screenHeight / 4, 
-            toggleFormVisibility: _toggleFormVisibility
-          ),
-          //movable form
-          if (_isFormVisible)
-            Positioned(
-              left: formPosition.dx,
-              top: formPosition.dy,
-              child: GestureDetector(
-                onPanUpdate: _handleFormUpdate,
-                child: gridForm(screenHeight * 0.25),
-              )
-            ),
-        ],
-      ),
+    return widget.toolBarButton(
+      Icons.grid_on,
+      _toggleFormVisibility,
+      Colors.grey.shade300,
     );
   }
-
-}
-/////////////CUSTOM TOOLBAR CLASS/////////////////////////
-class CustomToolBar extends StatefulWidget {
-    final double screenWidth;
-    final double screenHeight;
-    final double ypos;
-    final Function() toggleFormVisibility;
-
-    CustomToolBar({
-        required this.screenWidth,
-        required this.screenHeight,
-        required this.ypos,
-        required this.toggleFormVisibility,
-    });
-
-    @override
-    _CustomToolBarState createState() => _CustomToolBarState();
-}
-
-class _CustomToolBarState extends State<CustomToolBar> {
-    Offset toolbarPosition = Offset.zero;
-    bool paintSelected = false;
-    Color buttonColor = Colors.grey.shade300;
-
-    @override
-    initState() {
-      super.initState();
-      toolbarPosition = Offset(0, widget.ypos);
-    }
-    
-    void _togglePaintButton(){
-      setState(() {
-        paintSelected = !paintSelected;
-        if(!paintSelected){
-          buttonColor = Colors.grey.shade300;
-        } else {
-          buttonColor = Colors.blue.shade300;
-        }
-      });
-    }
-
-    void _handleToolBarUpdate(DragUpdateDetails details) {
-      setState(() {
-        toolbarPosition += details.delta;
-      });
-    }
-
-    Widget toolBarButton(IconData icon, Function() action, Color color) {
-        return Flexible(
-            flex: 1,
-            child: FractionallySizedBox(
-                widthFactor: 0.75,
-                heightFactor: 0.2,
-                child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16.0),
-                            color: color,
-                            boxShadow: [
-                                BoxShadow(
-                                    color: Colors.grey.withOpacity(0.5),
-                                    spreadRadius: 5,
-                                    blurRadius: 7,
-                                    offset: Offset(0, 3),
-                                ),
-                            ],
-                        ),
-                        child: GestureDetector(
-                            onTap: action,
-                            child: Icon(
-                                icon,
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        );
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      double width = widget.screenWidth * 0.1;
-      double height = widget.screenHeight * 0.5;
-
-      double threshold = widget.screenHeight * 0.05;
-      double lowerThreshold = widget.screenHeight - threshold - height;
-      bool rotatedBar = false;
-
-      if(toolbarPosition.dy <= threshold || toolbarPosition.dy >= lowerThreshold)
-      {
-        rotatedBar = true;
-      } else {
-        rotatedBar = false;
-      }
-
-        return Positioned(
-            left: toolbarPosition.dx,
-            top: toolbarPosition.dy,
-            child: GestureDetector(
-                onPanUpdate: _handleToolBarUpdate,
-                child: AnimatedRotation(
-                    turns: rotatedBar ? 0.25 : 0, 
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.easeInOut,
-                    child: Container(
-                        width: width,
-                        height: height,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16.0),
-                            color: Colors.grey[400],
-                        ),
-                        child: Padding(
-                            padding: const EdgeInsets.only(top: 12.0),
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                    toolBarButton(Icons.grid_on, widget.toggleFormVisibility, Colors.grey.shade300),
-                                    toolBarButton(Icons.format_paint, _togglePaintButton, buttonColor),
-                                ],
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        );
-    }
 }
 
