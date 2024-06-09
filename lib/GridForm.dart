1783df8360ccb73124ac4271bfc1174ae034b01f
TheRealRaziandJaso <Neno701@outlook.com>
2024-06-09 15:03:41 +1000

    fixed grid form bug

--- lib/GridForm.dart
+++ lib/GridForm.dart
@@ -51,107 +51,92 @@ class _GridFormState extends State<GridForm> {
             borderRadius: BorderRadius.circular(16.0),
             color: Colors.grey.shade400,
           ),
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    'Grid Form',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Grid Form',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              SizedBox(
+                width: height * 0.8,
+                child: TextField(
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  keyboardType: TextInputType.number,
+                  controller: _widthController,
+                  decoration: InputDecoration(
+                    labelText: 'Width',
+                    fillColor: Colors.grey.shade400,
+                    filled: true,
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: SizedBox(
-                    width: height * 0.8,
-                    child: TextField(
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      keyboardType: TextInputType.number,
-                      controller: _widthController,
-                      decoration: InputDecoration(
-                        labelText: 'Width',
-                        fillColor: Colors.grey.shade400,
-                        filled: true,
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                      ),
-                    ),
+              ),
+              SizedBox(
+                width: height * 0.8,
+                child: TextField(
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  keyboardType: TextInputType.number,
+                  controller: _heightController,
+                  decoration: InputDecoration(
+                    labelText: 'Height',
+                    fillColor: Colors.grey.shade400,
+                    filled: true,
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: SizedBox(
-                    width: height * 0.8,
-                    child: TextField(
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      keyboardType: TextInputType.number,
-                      controller: _heightController,
-                      decoration: InputDecoration(
-                        labelText: 'Height',
-                        fillColor: Colors.grey.shade400,
-                        filled: true,
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                      ),
-                    ),
-                  ),
+              ),
+              Container(
+                width: 100, // Adjust the width as needed
+                height: 50, // Adjust the height as needed
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade500, // Background color
+                    borderRadius: BorderRadius.circular(8.0), // Rounded corners
+                    boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // Shadow position
+                        ),
+                    ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Container(
-                    width: 100, // Adjust the width as needed
-                    height: 50, // Adjust the height as needed
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade500, // Background color
-                        borderRadius: BorderRadius.circular(8.0), // Rounded corners
-                        boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: Offset(0, 3), // Shadow position
-                            ),
-                        ],
-                    ),
-                    child: GestureDetector(
-                      onTap: () {
-                      final String widthInput = _widthController.text;
-                      final String heightInput = _heightController.text;
+                child: GestureDetector(
+                  onTap: () {
+                  final String widthInput = _widthController.text;
+                  final String heightInput = _heightController.text;
 
-                      int? width = int.tryParse(widthInput);
-                      int? height = int.tryParse(heightInput);
+                  int? width = int.tryParse(widthInput);
+                  int? height = int.tryParse(heightInput);
 
-                      // Check if the conversions were successful
-                      if (width != null && height != null) {
-                        if(width <= 200 && height <= 200){
-                          gridListCubit.addGrid(CreateGrid(width: width, height: height));
-                        }
-                      } else {
-                        print("aint work");
-                      }
-                      formCubit.changeFormVisibility();
-                      },
-                      child: Center(
-                          // Center the text within the container
-                          child: Text(
-                              'Submit',
-                              style: TextStyle(color: Colors.white),
-                          ),
+                  // Check if the conversions were successful
+                  if (width != null && height != null) {
+                    if(width <= 200 && height <= 200){
+                      gridListCubit.addGrid(CreateGrid(width: width, height: height));
+                    }
+                  } else {
+                    print("aint work");
+                  }
+                  formCubit.changeFormVisibility();
+                  },
+                  child: Center(
+                      // Center the text within the container
+                      child: Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.white),
                       ),
-                    )
                   ),
-                ),
-              ],
-            ),
+                )
+              ),
+            ],
           ),
         ),
       ),
     );
-  
   }
 }
